baa7cbbeadeb9d2113ce58dee0a1a6d61630a369
ynrrizki <yanuarrizki165@gmail.com>
2022-02-11 06:59:36 +0700

    sequintial search check with index

--- src/algorithms/SequintialSearch.java
+++ src/algorithms/SequintialSearch.java
@@ -1,19 +1,31 @@
 package algorithms;
 
 public class SequintialSearch {
-    static boolean sequintial(int listData[], int data) {
-        for(int i = 0; i < listData.length; i++) {
+    // Check dengan menampilkan indeks data nya
+    static int sequintialCheckIndex(int listData[], int data) {
+        for (int i = 0; i < listData.length; i++) {
+            if (data == listData[i]) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    // Check dengan boolean
+    static boolean sequintialCheck(int listData[], int data) {
+        for (int i = 0; i < listData.length; i++) {
             if (data == listData[i]) {
                 return true;
             }
         }
         return false;
     }
-    
+
     public static void main(String[] args) {
-        int listData[] = {12, 33, 5, 7, 9, 10};
-        int data = 1;
-        
-        System.out.println(sequintial(listData, data));
-    } 
+        int listData[] = { 12, 33, 5, 7, 9, 10 };
+        int data = 10;
+
+        System.out.println(sequintialCheck(listData, data));
+        System.out.println(sequintialCheckIndex(listData, data));
+    }
 }
